d2e8a3ec94642d7f773a029d2d6723a8952960cb
Dheeraj Vagavolu <41289602+dheerajVagavolu@users.noreply.github.com>
2023-09-30 20:25:28 -0400

    new scripts

---
+++ new/Assets/Samples/OpenAI Unity/0.1.15/Whisper/Whisper.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+
+namespace OpenAI
+{
+    public class Whisper : MonoBehaviour
+    {
+        [SerializeField] private Button recordButton;
+        [SerializeField] private Image progressBar;
+        [SerializeField] private Text message;
+        [SerializeField] private Dropdown dropdown;
+
+
+        [SerializeField] private RectTransform sent;
+        [SerializeField] private RectTransform received;
+        private float height;
+        [SerializeField] private ScrollRect scroll;
+        private List<ChatMessage> messages = new List<ChatMessage>();
+        private string prompt = "You're a recent grad from the University of Waterloo. You studied computer science and signed full time as a Machine Learning Engineer making $120k. After expenses & taxes, they are left with $15,246 a year. Your goal is to buy a house and am coming to me looking for financial advice. I am a financial advisor.\nYou are a reckless spender. You spend on sushi, fashion trends, and rock climbing.\nYou don't know anything about finance! Say you don't know if asked about finances. You don't know if any are risky or not. You will accept my suggestions without question. You will not think for yourself. Respond not too long.\n Ask for my help!";
+
+        private void AppendMessage(ChatMessage message)
+        {
+            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
+
+            var item = Instantiate(message.Role == "user" ? sent : received, scroll.content);
+            item.GetChild(0).GetChild(0).GetComponent<Text>().text = message.Content;
+            item.anchoredPosition = new Vector2(0, -height);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(item);
+            height += item.sizeDelta.y;
+            scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+            scroll.verticalNormalizedPosition = 0;
+        }
+
+        private readonly string fileName = "output.wav";
+        private readonly int duration = 5;
+        
+        private AudioClip clip;
+        private bool isRecording;
+        private float time;
+        private OpenAIApi openai = new OpenAIApi();
+
+        private void Start()
+        {
+            #if PLATFORM_ANDROID
+            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+            {
+                Permission.RequestUserPermission(Permission.Microphone);
+                //dialog = new GameObject();
+            }
+            #endif
+            Debug.Log(Microphone.devices.ToString());
+            #if UNITY_WEBGL && !UNITY_EDITOR
+                        dropdown.options.Add(new Dropdown.OptionData("Microphone not supported on WebGL"));
+            #else
+            Debug.Log(Microphone.devices.Length);
+            foreach (var device in Microphone.devices)
+            {
+                dropdown.options.Add(new Dropdown.OptionData(device));
+            }
+            recordButton.onClick.AddListener(StartRecording);
+            dropdown.onValueChanged.AddListener(ChangeMicrophone);
+            
+            var index = PlayerPrefs.GetInt("user-mic-device-index");
+            dropdown.SetValueWithoutNotify(index);
+            #endif
+        }
+
+        private void ChangeMicrophone(int index)
+        {
+            PlayerPrefs.SetInt("user-mic-device-index", index);
+        }
+        
+        private void StartRecording()
+        {
+            isRecording = true;
+            recordButton.enabled = false;
+
+            var index = PlayerPrefs.GetInt("user-mic-device-index");
+            
+            #if !UNITY_WEBGL
+            clip = Microphone.Start(dropdown.options[index].text, false, duration, 44100);
+            #endif
+        }
+
+        private async void EndRecording()
+        {
+            message.text = "Transcripting...";
+            
+            #if !UNITY_WEBGL
+            Microphone.End(null);
+            #endif
+            
+            byte[] data = SaveWav.Save(fileName, clip);
+            
+            var req = new CreateAudioTranscriptionsRequest
+            {
+                FileData = new FileData() {Data = data, Name = "audio.wav"},
+                // File = Application.persistentDataPath + "/" + fileName,
+                Model = "whisper-1",
+                Language = "en"
+            };
+            var res = await openai.CreateAudioTranscription(req);
+
+            progressBar.fillAmount = 0;
+            message.text = res.Text;
+
+
+
+            var newMessage = new ChatMessage()
+            {
+                Role = "user",
+                Content = message.text
+            };
+
+            AppendMessage(newMessage);
+
+            if (messages.Count == 0) newMessage.Content = message.text + "\n" + prompt;
+
+            messages.Add(newMessage);
+
+            // Complete the instruction
+            var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
+            {
+                Model = "gpt-3.5-turbo-0613",
+                Messages = messages
+            });
+
+            if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
+            {
+                var message = completionResponse.Choices[0].Message;
+                message.Content = message.Content.Trim();
+
+                messages.Add(message);
+                AppendMessage(message);
+            }
+            else
+            {
+                Debug.LogWarning("No text was generated from this prompt.");
+            }
+
+            recordButton.enabled = true;
+        }
+
+        private void Update()
+        {
+            if (isRecording)
+            {
+                time += Time.deltaTime;
+                progressBar.fillAmount = time / duration;
+                
+                if (time >= duration)
+                {
+                    time = 0;
+                    isRecording = false;
+                    EndRecording();
+                }
+            }
+        }
+    }
+}
